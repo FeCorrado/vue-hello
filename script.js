a3c238080b188e7d27c68b5e6e8c93bb6cac6828
Corrado.Ferra <corradoferra@gmail.com>
2022-01-13 18:49:29 +0100

    Done + resti di prove bonus    :'(

---
+++ script.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: "#app",
+    data:{
+        messaggio:'Hello world',
+        picture: 'https://unsplash.it/600/300?image=142',
+        nome: 'inserisci il tuo nome',  
+        // picture2: 'https://unsplash.it/600/300?image=',
+    },
+
+    /*
+    methods: {
+        saluta: function() {
+            allert(this.nome)
+        }
+    },
+    */
+});
